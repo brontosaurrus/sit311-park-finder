2e646bc3add7443dd072f7f884d93e4809354ff8
Nishnath <nalahakone@deain.edu.au>
2019-09-23 22:45:31 +1000

    Updated markers. Formatted Weather

--- src/WeatherInfoComponent.js
+++ src/WeatherInfoComponent.js
@@ -8,12 +8,13 @@ class WeatherInfoComponent extends React.Component {
 				//<p>{JSON.stringify(this.props.weather)}</p>
 			//</React.Fragment>
 			<div>
-				{this.props.city && <p>Location: {this.props.city}</p>}
-				{this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-				{this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-				{this.props.pressure && <p>Pressure: {this.props.pressure}</p>}
-				{this.props.wind && <p>Wind: speed is {this.props.wind.speed}km/h and direction is {this.props.wind.deg} degrees</p>}
-				{this.props.cloudiness && <p>Cloudiness: {this.props.cloudiness}</p>}
+				{this.props.city && <p>Location:  {this.props.city}</p>}
+				{this.props.temperature && <p><b>Temperature </b>  {this.props.temperature} °C</p>}
+				{this.props.humidity && <p><b> Humidity </b>{this.props.humidity} %</p>}
+				{this.props.pressure && <p><b>Pressure </b> {this.props.pressure} hpa</p>}
+				{this.props.wind && <p><b>Wind Speed </b>{this.props.wind.speed}km/h</p>}
+				{this.props.wind && <p><b>Direction </b>{this.props.wind.deg}°</p>}
+				{this.props.cloudiness && <p><b>Cloudiness </b> {this.props.cloudiness}</p>}
 			</div>
 		)
 	}
